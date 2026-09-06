200778fef91f568b8e6a2f9871339527f4a8a336
feixuan909-tech <feixuan909@gmail.com>
2026-09-06 18:07:01 +0800

    Add files via upload

--- hw-offline-sw.js
+++ hw-offline-sw.js
@@ -1,4 +1,4 @@
-const CACHE='hw-checker-r8-sandbox-datalogicfix9-shell-v1';
+const CACHE='hw-checker-r8-sandbox-datalogicfix10-shell-v1';
 self.addEventListener('install',event=>{self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{
   const keys=await caches.keys();
